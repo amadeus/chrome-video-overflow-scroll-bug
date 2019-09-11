072808e4717227e3e263b6b6cc5b37f168a05008
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2019-09-10 18:33:13 -0700

    Basic example of bug

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, {useState} from 'react';
+import {render} from 'react-dom';
+import styles from './Styles.module.css';
+import videoURL from './vid.mp4';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Example = () => {
+  const [visibility, setVisibility] = useState('visible');
+  const handleClick = () => setVisibility(visibility === 'visible' ? 'hidden' : 'visible');
+  return (
+    <div className={styles.scroller}>
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+        standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make
+        a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
+        remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
+        Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+      </p>
+      <button onClick={handleClick}>Toggle Video Visibility</button>
+      <video src={videoURL} autoPlay loop height={100} style={{visibility}} muted />
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+        standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make
+        a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
+        remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
+        Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+      </p>
+    </div>
+  );
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+render(<Example />, document.getElementById('root'));
