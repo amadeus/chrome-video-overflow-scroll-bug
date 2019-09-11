e6fdaacb49fa62fa60a74cccef2e05dc4bdb1d26
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2019-09-10 18:45:25 -0700

    Adding React Helmet

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import {Helmet} from 'react-helmet';
 import {render} from 'react-dom';
 import styles from './Styles.module.css';
 import videoURL from './vid.mp4';
@@ -8,6 +9,9 @@ const Example = () => {
   const handleClick = () => setVisibility(visibility === 'visible' ? 'hidden' : 'visible');
   return (
     <div className={styles.scroller}>
+      <Helmet>
+        <title>Chrome Subpixel Font Rendering Bug</title>
+      </Helmet>
       <p>
         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
         standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make
